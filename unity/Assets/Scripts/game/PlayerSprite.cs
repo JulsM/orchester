6eedfe072b7d1cf16295b68e461daa238175ed05
Matthias Frauer <s9mafrau@stud.uni-saarland.de>
2016-01-03 14:40:12 +0100

    playerSprite should now spawn sphere on screen when created

--- unity/Assets/Scripts/game/PlayerSprite.cs
+++ unity/Assets/Scripts/game/PlayerSprite.cs
@@ -5,12 +5,18 @@ public class PlayerSprite : MonoBehaviour {
 
     private string id;
     private float y;
+    private GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    private Material myMaterial = new Material(Shader.Find("Particles/Alpha Blended"));
 
-    public PlayerSprite( string id, float y)
+
+    public PlayerSprite( string id, float y) // Color c)
     {
         this.id = id;
         this.y = y;
-        //TODO create drawable unity GameObject itself
+        sphere.transform.position = new Vector3(0, 0, 0);
+        sphere.transform.localScale = new Vector3(1, 1, 1);
+        myMaterial.color = Color.blue;
+        sphere.GetComponent<MeshRenderer>().material = myMaterial;
         //TODO how to remove? maybe seperate 'delete' function to be called before deleting it in the DrawOnScreen - Scripts map
     }
 
@@ -18,6 +24,16 @@ public class PlayerSprite : MonoBehaviour {
 	void Update () {
 	    //TODO this for animation?
 	}
+
+    public void changeColor(Color c)
+    {
+        myMaterial.color = c;
+    }
+
+    public void deleteSphere()
+    {
+        sphere.SetActive(false);
+    }
 
     public string getID()
     {
