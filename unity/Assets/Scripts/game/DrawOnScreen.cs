3ecb3210a773ee3defee7995e8a665a3692d5422
Matthias Frauer <s9mafrau@stud.uni-saarland.de>
2016-01-12 14:55:05 +0100

    code cleanup

--- unity/Assets/Scripts/game/DrawOnScreen.cs
+++ unity/Assets/Scripts/game/DrawOnScreen.cs
@@ -4,10 +4,6 @@ using System.Collections.Generic;
 
 public class DrawOnScreen : MonoBehaviour {
 
-    //private float x;
-    Vector3 startPosition = new Vector3 (-10,0,5);
-    int maxSpeed=3;
-    int neg = 1;
 
     void Start () {
 
@@ -15,19 +11,8 @@ public class DrawOnScreen : MonoBehaviour {
 
     public void draw(Player p, PlayerSprite s)
     {
-        
         s.Y = p.Y/10;
-        //s.getSphere().transform.position = new Vector3(0, 0, p.Y / 10);
-        //s.getSphere().transform.position = new Vector3(startPosition.x + (Mathf.Sin(Time.deltaTime)*neg), 0, p.Y/10);
         s.getSphere().transform.position = new Vector3(Mathf.PingPong(Time.time, 10)-5 , 0, p.Y / 10);
- //       if (s.getSphere().transform.position.x > 10f || transform.position.x < -10f)
- /*       {
-            neg = -neg;
-            //transform.position = new Vector3(transform.position.x, 0, p.Y/10);
-        }*/
-        //Debug.Log("xPos: " + startPosition.x + Mathf.Sin(Time.time * maxSpeed));
-//        Debug.Log("s.Y: " + s.Y);
-//        Debug.Log("p.Y: " + p.Y);
     }
 
 
