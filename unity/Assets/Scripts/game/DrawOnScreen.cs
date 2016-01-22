e05b1c23e15f756ccf7f6e56bb4525d9b36a13e6
Matthias Frauer <s9mafrau@stud.uni-saarland.de>
2016-01-22 17:06:35 +0100

    now spawns and destroys spriteanimation

--- unity/Assets/Scripts/game/DrawOnScreen.cs
+++ unity/Assets/Scripts/game/DrawOnScreen.cs
@@ -4,7 +4,13 @@ using System.Collections.Generic;
 
 public class DrawOnScreen : MonoBehaviour {
 
+    int i,y;
+    GameObject[] explosions;
+
     void Start () {
+        i = 0;
+        y = 0;
+        explosions = new GameObject[5];
     }
 
     public void draw(Player p, PlayerSprite s)
@@ -12,6 +18,16 @@ public class DrawOnScreen : MonoBehaviour {
         s.Y = p.Y/10;
         s.getSphere().transform.position = new Vector3(Mathf.PingPong(Time.time *3, 24)-12 , 0, p.Y / 10);
         s.changeColor(p.Instrument);
+        if (i == 30)
+        {
+            if (y >= 5) y = 0;
+            GameObject.Destroy(explosions[y]);
+            explosions[y] = s.addAfterImage();
+            explosions[y].transform.position = s.getSphere().transform.position;
+            y++;
+            i = 0;
+        }
+        i++;
         //GameObject ai = s.addAfterImage();
         //ai.transform.position = s.getSphere().transform.position;
         //s.OnCollisionEnter();
