deaaa81e20b14ebaa10b2a0cadd27519833f2c8d
Matthias Frauer <s9mafrau@stud.uni-saarland.de>
2016-01-26 15:21:16 +0100

    movement for 1024*768

--- unity/Assets/Scripts/game/DrawOnScreen.cs
+++ unity/Assets/Scripts/game/DrawOnScreen.cs
@@ -13,7 +13,7 @@ public class DrawOnScreen : MonoBehaviour {
     public void draw(Player p, PlayerSprite s)
     {
         s.Y = p.Y/10;
-		s.getSphere().transform.position = new Vector3(Mathf.PingPong(Time.time *3,24)-12 , 0, p.Y / 10);
+		s.getSphere().transform.position = new Vector3(Mathf.PingPong(Time.time *3,12)-6 , 0, p.Y / 10);// 24  -12
         s.changeColor(p.Instrument);
         if (wait == 20)
         {
